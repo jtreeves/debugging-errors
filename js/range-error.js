d4a9d32bd308dc5b18e4eb78dc5a2fc67145e257
jtreeves <jackson.t.reeves@gmail.com>
2020-10-30 15:15:19 -0400

    [range-error] Edit my comments to clarify the scenarios and solutions

--- js/range-error.js
+++ js/range-error.js
@@ -15,31 +15,39 @@ RESOURCE: CHECK YOUR README 📖
 
 // Error 1
 // Invalid:
-3.57.toFixed(-99);
+3.57.toFixed(-9);
 
 // Problem:
-// RangeError: toFixed() argument must be between 0 and 100; line 18 (range-error.js:15); type of "precision is out of range"
+// RangeError: toFixed() argument must be between 0 and 100; line 18 (range-error.js:18); type of "precision is out of range"
+
+// Solution:
+// Make the argument a positive integer
 
 // Valid:
-3.57.toFixed(99);
+3.57.toFixed(9); // '3.570000000'
 
 // Error 2
 // Invalid:
-(975).toString(-1);
+(975).toString(-10);
 
 // Problem:
-// RangeError: toString() radix argument must be between 2 and 36; line 28 (range-error.js:28); type of "radix must be an integer"
+// RangeError: toString() radix argument must be between 2 and 36; line 31 (range-error.js:31); type of "radix must be an integer"
+
+// Solution:
+// Make the argument a positive integer between 2 and 36
 
 // Valid:
-(975).toString(10);
-
+(975).toString(10); // '975'
 
 // Error 3
 // Invalid:
 'I am a string'.repeat(-7);
 
 // Problem:
-// RangeError: String.prototype.repeat argument must be greater than or equal to 0 and not be Infinity; line 39 (range-error:js.39); type of "repeat count must be non-negative"
+// RangeError: String.prototype.repeat argument must be greater than or equal to 0 and not be Infinity; line 44 (range-error:js.44); type of "repeat count must be non-negative"
+
+// Solution:
+// Make the argument a positive integer
 
 // Valid:
-'I am a string'.repeat(7);
+'I am a string'.repeat(7); // 'I am a stringI am a stringI am a stringI am a stringI am a stringI am a stringI am a string'
